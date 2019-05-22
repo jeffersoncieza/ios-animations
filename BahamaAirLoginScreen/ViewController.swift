5b0955a1d1383901787622ae3821ecda587c4b09
Jeferson Cieza <jcieza@sudtechnologies.com>
2019-05-22 09:41:21 -0500

    animations

--- BahamaAirLoginScreen/ViewController.swift
+++ BahamaAirLoginScreen/ViewController.swift
@@ -77,10 +77,29 @@ class ViewController: UIViewController {
   
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
+    
+    heading.center.x -= view.bounds.width
+    username.center.x -= view.bounds.width
+    password.center.x -= view.bounds.width
   }
   
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
+    
+    UIView.animate(withDuration: 0.5) {
+        self.heading.center.x += self.view.bounds.width
+    }
+    
+    UIView.animate(withDuration: 0.5, delay: 0.3, options: [],
+                   animations: {
+        self.username.center.x += self.view.bounds.width
+    }, completion: nil)
+    
+    UIView.animate(withDuration: 0.5, delay: 0.4,
+                   options: [],
+                   animations: {
+        self.password.center.x += self.view.bounds.width
+    }, completion: nil)
   }
   
   // MARK: further methods
@@ -96,5 +115,4 @@ class ViewController: UIViewController {
     nextField?.becomeFirstResponder()
     return true
   }
-  
 }
